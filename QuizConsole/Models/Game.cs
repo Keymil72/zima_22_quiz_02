2cdd504b7003a87a71c11da65f1aeef1dceec6cd
Keymil72 <kamip@DESKTOP-SLFES5R>
2022-11-28 21:13:58 +0100

    Dodaj pliki projektów.

---
+++ QuizConsole/Models/Game.cs
@@ -0,0 +1,95 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizConsole.Models
+{
+    public class Game
+    {
+        public Game()
+        {
+            
+            GetQuestions();
+            Random = new Random();
+            Categories = AllQuestions
+                .Select(x => x.Category)
+                .OrderBy(x => x)
+                .Distinct().ToList();
+
+            //Categories = new List<int> { 100, 200, 300, 400, 500, 750, 1000 };
+            CurrentCategory = Categories[CurrentCategoryIndex];
+
+            IsWheelAvailable= true;
+        }
+
+
+        // właściwości
+        public List<Question> AllQuestions { get; set; }
+        public int CurrentCategory { get; set; }
+        public Question CurrentQuestion { get; set; }
+        public Random Random { get; set; }
+        public List<int> Categories { get; set; }
+        public int CurrentCategoryIndex { get; set; }
+        public bool IsWheelAvailable { get; set; }
+
+        public List<int> CorrectKeys = new List<int> { 1, 2, 3, 4 };
+
+
+        // metoda tworzaca bazę pytań
+        void GetQuestions()
+        {
+            var path = $"{Directory.GetCurrentDirectory()}\\questions.json";
+            var json = File.ReadAllText(path);
+            AllQuestions = JsonConvert.DeserializeObject<List<Question>>(json);
+        }
+
+        // metoda losująca jedno pytanie z wszystkich pytan aktualnej kategorii
+        public void DrawQuestion()
+        {
+            var questionsCat = AllQuestions.Where(q => q.Category == CurrentCategory).ToList();
+            var number = Random.Next(0, questionsCat.Count);
+            var selectedQuestion = questionsCat[number];
+            selectedQuestion.Answers = selectedQuestion.Answers.OrderBy(a => Random.Next()).ToList();
+            int index = 1;
+            foreach (var a in selectedQuestion.Answers)
+            {
+                a.DisplayOrder = index;
+                index++;
+            }
+            
+            CurrentQuestion = selectedQuestion;
+        }
+
+        public bool CheckIfAnswerCorrect(int playerAnswer)
+        {
+            return CurrentQuestion.Answers.First(a => a.DisplayOrder == playerAnswer).IsCorrect;
+        }
+
+        public bool CheckIfLastQuestion()
+        {
+            if (CurrentCategoryIndex == 6) return true;
+            else
+            {
+                CurrentCategoryIndex++;
+                CurrentCategory = Categories[CurrentCategoryIndex];
+                return false;
+            }
+        }
+
+        public void GetQuestionWithTwoAnswers()
+        {
+            var badAnswers = CurrentQuestion.Answers.Where(a => !a.IsCorrect);
+            var removed = badAnswers.OrderBy(a => Random.Next()).Take(2).ToList();
+            CorrectKeys.Remove(removed[0].DisplayOrder);
+            CorrectKeys.Remove(removed[1].DisplayOrder);
+            CurrentQuestion.Answers.Remove(removed[0]);
+            CurrentQuestion.Answers.Remove(removed[1]);
+            
+        }
+    }
+}
+
+// 100, 200, 300, 400, 500, 750, 1000
---
+++ QuizConsole/Models/Question.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizConsole.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int Category { get; set; }
+        public List<Answer> Answers { get; set; } = new List<Answer>();
+        public string Author { get; set; }
+        
+
+        public int Display(bool isWheelAvailable, List<int> correctKeys)
+        {
+            Console.Clear();
+            while (true)
+            {
+                QuestionText(isWheelAvailable);
+                var answer = Console.ReadLine();
+                if (IsCorrectKey(answer, isWheelAvailable, correctKeys) != 0)
+                {
+                    Console.WriteLine(IsCorrectKey(answer, isWheelAvailable, correctKeys));
+                    return IsCorrectKey(answer, isWheelAvailable, correctKeys);
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Wcisnałaś/eś nieprawidłowy klawisz ...");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+        }
+
+        private void QuestionText(bool isWheelAvailable)
+        {
+            Console.WriteLine($"Pytanie za {Category} pkt");
+            Console.WriteLine($"Autor: {Author}");
+            Console.WriteLine();
+            Console.WriteLine(Content);
+            Console.WriteLine();
+
+            foreach (var answer in Answers)
+                Console.WriteLine($"{answer.DisplayOrder}. {answer.Content}");
+
+            Console.WriteLine();
+            var message = isWheelAvailable
+                ? "Naciśnij 1, 2, 3, 4 lub K aby użyć koła  => "
+                : "Naciśnij 1, 2, 3 lub 4 => ";
+
+            Console.Write(message);
+
+        }
+
+
+        private int IsCorrectKey(string key, bool isWheelAvailable, List<int> correctKeys)
+        {
+            if ((key.ToLower() == "k") && isWheelAvailable) return 5;
+            if (int.TryParse(key, out int x) && correctKeys.Contains(x)) return x;
+            return 0;
+        }
+
+    }
+}
